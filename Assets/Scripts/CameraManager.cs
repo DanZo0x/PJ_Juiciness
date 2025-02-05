ed422c0b6db4732bedd20d51b26fa995f1943ee0
DisturbedBanana <tompouts@gmail.com>
2025-02-05 19:59:54 +0100

    [ADD] Assigned Art assets to gamescene + shoot particle feedback // FIX PROJECTILES PREFABS

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -8,15 +8,23 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private float duration = 0.5f; // Duration of the shake
     [SerializeField] private float strength = 1f; // Strength of the shake
     [SerializeField] private int vibrato = 10; // Vibrato (frequency) of the shake
-    [SerializeField] private float randomness = 90f; // Randomness of the shake
+    [SerializeField] private float randomness = 20f; // Randomness of the shake
+
+    private Vector3 originalPos;
 
     private void Awake()
     {
         Instance = this;
+        originalPos = Camera.main.transform.position;
     }
 
     public void ShakeCamera()
     {
-        Camera.main.transform.DOShakePosition(duration, strength, vibrato, randomness);
+        Camera.main.transform.DOShakePosition(duration, strength, vibrato, randomness).OnComplete(ResetPos);
+    }
+
+    private void ResetPos()
+    {
+        Camera.main.transform.position = originalPos;
     }
 }
